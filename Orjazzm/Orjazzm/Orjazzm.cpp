49aa73337513eccea3fee131b6744c2f238bb685
GrafEnters <43163351+GrafEnters@users.noreply.github.com>
2018-12-30 19:22:14 +0300

    Added Story, Vines and BIG rooms
    How will i finish the story?
    We will discover soon.

--- Orjazzm/Orjazzm/Orjazzm.cpp
+++ Orjazzm/Orjazzm/Orjazzm.cpp
@@ -12,9 +12,11 @@ using namespace std;
 const char simv_wall = char(219); // '▓' стена
 const char simv_floor = char(32); // ' ' проход
 const char simv_exit = char(88);  // 'X' выход
+const char simv_kust = char(176); // '░' куст
 const int skinamount = 6;
 const char hero_skins[skinamount] = { 'A', 'B', 'C','I','@','?' };   // 'A' 'B' 'C' герой
 const char simv_check = char(46);  // '.' проверка
+bool dangers[3] = { false,false,false };
 struct Hero
 {
 public:
@@ -54,6 +56,7 @@ class Level
 {
 public:
 	int width, height;
+	bool areyoubig;
 	int exitx, exity;
 	Hero hero;
 	char **pole;
@@ -61,13 +64,21 @@ public:
 
 	Level(Hero h, int number, int steps, double score, double bestscore)
 	{
+		areyoubig = false;
+		srand(time(0));
 		hero = h;
 		lvlnumber = number;
 		height = 10 + number / 3;
 		width = 10 + number / 3;
+		if (dangers[1] && rand() % 100 + lvlnumber > 85) // шанс появления огромного уровня 
+		{
+			areyoubig = true;
+			height *= 2;
+			width *= 2;
+		}
 		Create();
 
-		Show(steps, score, bestscore);
+		Show(steps, score, bestscore, areyoubig);
 	}
 	void Create()
 	{
@@ -90,6 +101,7 @@ public:
 				if (pole[i + 1][j] == simv_wall && pole[i - 1][j] == simv_wall && pole[i][j + 1] == simv_wall && pole[i - 1][j] == simv_wall)pole[i][j] = simv_wall;
 			}
 		}
+
 		pole[hero.x][hero.y] = hero.name;
 
 		bool proverka = false;
@@ -106,34 +118,67 @@ public:
 			pole[exitx][exity] = simv_exit;
 			proverka = PlaceFinish(pole);
 		}
+		if (dangers[0] && rand() % 3 == 1)
+		{
+			for (int i = 1; i < width - 1; i++)
+			{
+				for (int j = 1; j < height - 1; j++)
+				{
+					if (rand() % 4 == 1 && pole[i][j] == simv_floor)
+					{
+						pole[i][j] = simv_kust;
+					}
+					if (rand() % 2 == 1 && pole[i][j] == simv_floor && (pole[i + 1][j] == simv_kust || pole[i - 1][j] == simv_kust || pole[i][j + 1] == simv_kust && pole[i][j - 1] == simv_kust))
+					{
+						pole[i][j] = simv_kust;
+					}
+				}
+			}
+		}
 	}
 
-	void Show(int steps, double score, double bestscore)
+	void Show(int steps, double score, double bestscore, bool areyoubig)
 	{
-		changebuffersize(4 * width + 10, height + 10);
+		if (!areyoubig)changebuffersize(4 * width + 10, height + 10); else changebuffersize((4 * width + 10) / 2, (height + 10) / 2);
 		cout << endl;
 		system("cls");
-		for (int i = 0; i < width; i++)
-		{
-			for (int j = 0; j < height; j++)
-				cout << pole[i][j] << pole[i][j];
-			if (i == 0) cout << "           Level: " << lvlnumber;
-			if (i == 2) cout << setprecision(3) << fixed << "         SCORE: " << score;
-			if (i == 3) cout << setprecision(3) << fixed << "       bestscore: " << bestscore;
-			if (i == 6) cout << "           Steps: " << steps;
-			if (i == width - 2) cout << "     Press 0 to end game ";
-			cout << endl;
+		if (areyoubig) {
+			int inach = 0, jnach = 0;
+			if (hero.x >= width / 2 && hero.y <= height / 2) { inach = width / 2; jnach = 0; }
+			if (hero.x <= width / 2 && hero.y >= height / 2) { inach = 0; jnach = height / 2; }
+			if (hero.x >= width / 2 && hero.y >= height / 2) { inach = width / 2; jnach = height / 2; }
+
+			for (int i = inach; i < inach + width / 2; i++)
+			{
+				for (int j = jnach; j < jnach + height / 2; j++)
+					cout << pole[i][j] << pole[i][j];
+				if (i == inach + 0) cout << "           Level: " << lvlnumber;
+				if (i == inach + 2) cout << setprecision(3) << fixed << "         SCORE: " << score;
+				if (i == inach + 3) cout << setprecision(3) << fixed << "       bestscore: " << bestscore;
+				if (i == inach + 6) cout << "           Steps: " << steps;
+				if (i == inach + width / 2 - 2) cout << "     Press 0 to end game ";
+				cout << endl;
+			}
 		}
+		else
+			for (int i = 0; i < width; i++)
+			{
+				for (int j = 0; j < height; j++)
+					cout << pole[i][j] << pole[i][j];
+				if (i == 0) cout << "           Level: " << lvlnumber;
+				if (i == 2) cout << setprecision(3) << fixed << "         SCORE: " << score;
+				if (i == 3) cout << setprecision(3) << fixed << "       bestscore: " << bestscore;
+				if (i == 6) cout << "           Steps: " << steps;
+				if (i == width - 2) cout << "     Press 0 to end game ";
+				cout << endl;
+			}
 	}
 
 	bool PlaceFinish(char **f)
 	{
 		bool flag = false;
-		//тут должна быть проверка на достижимостть финиша, но пока я не знаю как это сделать 
-		//ни гугл, ни обычный волновой тут не помогут, т.к. у меня зациклено поле 
-		//так что ожидаемо в среду я это сделаю, пока нет ни времени, ни желания
 
-		int nwas, mwas;
+		int nwas, mwas; char was;
 		for (int n = 0; n < width; n++)
 		{
 			for (int m = 0; m < height; m++)
@@ -261,11 +306,12 @@ int main()
 	changefontsize(20);
 	Hero A;
 
+	int bonus = 0;
 	int steps = 0;
 	int lvlnumber = 0;
 	double score = 0, bestscore = 0;
 	bool skins[skinamount] = { true,false,false,false,false,false };
-	bool dangers[3] = { false,false,false };
+	bool newpage = false, newskin = false;
 	savefile >> bestscore >> skins[5];
 	char firsttime;
 
@@ -286,7 +332,7 @@ int main()
 
 	string pos[skinamount] = { "  ","  ","  ","  ", "  ","  " };
 
-	if (bestscore >= 5) { dangers[0] = true; }
+	if (bestscore >= 3) { dangers[0] = true; }
 	if (bestscore >= 10) { skins[1] = true; dangers[1] = true; }
 	if (bestscore >= 25) { skins[2] = true; dangers[2] = true; }
 	if (bestscore >= 50) { skins[3] = true; }
@@ -303,8 +349,12 @@ int main()
 				<< "           ORJAZZM          " << endl
 				<< "                            " << endl
 				<< "      <1> Start Game        " << endl
-				<< "      <2> Rules             " << endl
-				<< "      <3> Skins             " << endl
+				<< "      <2> Story   ";
+			if (newpage) cout << "++";
+			cout << endl
+				<< "      <3> Skins   ";
+			if (newskin) cout << "++";
+			cout << endl
 				<< "                            " << endl
 				<< "         <0> Quit           " << endl
 				<< endl
@@ -314,7 +364,10 @@ int main()
 			if (direction == '0') direction = '!';
 			if (direction == '?')
 			{
-				skins[3] = true; ofstream sf("Save.txt"); sf << bestscore << " " << skins[5]; sf.close();
+				skins[5] = true;
+				ofstream sf("Save.txt"); sf << bestscore << " " << skins[5];
+				sf.close();
+				newskin = true;
 			}
 			break;
 		}
@@ -343,8 +396,8 @@ int main()
 			while (direction != '0')
 			{
 				Level lvl(A, lvlnumber, steps, score, bestscore);
-				m = 10 + lvlnumber / 3;
-				n = 10 + lvlnumber / 3;
+				m = lvl.width;
+				n = lvl.height;
 
 				// передвижение
 				while (direction != '0' && lvl.hero.memory != simv_exit)
@@ -377,11 +430,11 @@ int main()
 						lvl.hero.x = xnew; lvl.hero.y = ynew;
 						lvl.hero.memory = lvl.pole[xnew][ynew];
 					}
-					lvl.pole[lvl.hero.x][lvl.hero.y] = lvl.hero.name;
+					if (lvl.hero.memory != simv_kust)  lvl.pole[lvl.hero.x][lvl.hero.y] = lvl.hero.name;
 					if (direction != '0')
 					{
 						steps++;
-						score = lvlnumber * (1 + (double)lvlnumber / 100.0) - (double)steps / (10 + 2 * lvlnumber / 3);
+						score = lvlnumber * (1 + (double)lvlnumber / 100.0) - (double)steps / (10 + 2 * lvlnumber / 3) + bonus;
 						if (score > bestscore)
 						{
 							bestscore = score;
@@ -389,19 +442,27 @@ int main()
 							sf << bestscore << " " << skins[5];
 							sf.close();
 						}
-						if (bestscore >= 5) { dangers[0] = true; }
-						if (bestscore >= 1) { skins[1] = true; dangers[1] = true; }
-						if (bestscore >= 25) { skins[2] = true; dangers[2] = true; }
+						if (bestscore >= 5) { dangers[0] = true; newpage = true; }
+						if (bestscore >= 10) { skins[1] = true; newskin = true; dangers[1] = true; newpage = true; }
+						if (bestscore >= 25) { skins[2] = true; newskin = true; }
+						if (bestscore >= 50) { skins[3] = true; newskin = true; }
+						if (bestscore >= 100) { skins[4] = true; newskin = true; }
 					}
-					lvl.Show(steps, score, bestscore);
+					lvl.Show(steps, score, bestscore, lvl.areyoubig);
 				}
 				lvlnumber++;
-				lvl.Show(steps, score, bestscore);
+				if (lvl.areyoubig)
+				{
+					bonus += 5;
+					score = lvlnumber * (1 + (double)lvlnumber / 100.0) - (double)steps / (10 + 2 * lvlnumber / 3) + bonus;
+				}
+				lvl.Show(steps, score, bestscore, lvl.areyoubig);
 			}
 			break;
 		}
-		case '2': // rules pages
+		case '2': // story pages
 		{
+			newpage = false;
 			int page = 1;
 			while (direction != '0')
 			{
@@ -410,39 +471,93 @@ int main()
 				{
 				case 1:
 					cout
-						<< "           Page 1/2         " << endl
-						<< "                             " << endl
-						<< "    Controls:      W         " << endl
-						<< "               A   S   D     " << endl
-						<< "    0 - Back/Quit            " << endl
-						<< "                             " << endl
-						<< "           <0> Back       <d>" << endl
+						<< "           Page 1/4             " << endl
+						<< "                                " << endl
+						<< "    Hate those dumb guards.     " << endl
+						<< "    I WILL rule this kingdom.   " << endl
+						<< "    Good way or another.        " << endl
+						<< "       <use WASD to move>       " << endl
+						<< "           <0> Back          <d>" << endl
 						<< endl
 						<< "             ";
 					break;
 				case 2:
 					cout
-						<< "           Page 2/2         " << endl
-						<< "                             " << endl
-						<< "    XX   <- Exit                   " << endl
-						<< "                 Wall  ->   ▓  " << endl
-						<< "    AA   <-  You               " << endl
-						<< "                             " << endl
-						<< "<a>        <0> Back          " << endl
+						<< "           Page 2/4             " << endl
+						<< "                                " << endl
+						<< "    Hope, gossips do not lie    " << endl
+						<< "    At the bottom of city sewers " << endl
+						<< "    Lies old graveyard.         " << endl
+						<< "    Time to meet my old friend. " << endl
+						<< "<a>        <0> Back          <d>" << endl
 						<< endl
 						<< "             ";
+					break;
+				case 3:
+					if (dangers[0]) {
+						cout
+							<< "           Page 3/4             " << endl
+							<< "                                " << endl
+							<< "     Dense VINES appears        " << endl
+							<< "     More and more often.       " << endl
+							<< "     How long am I wandering    " << endl
+							<< "     through this MAZES ?       " << endl
+							<< "<a>        <0> Back          <d>" << endl
+							<< endl
+							<< "             ";
+					}
+					else
+					{
+						cout
+							<< "           Page 3/4             " << endl
+							<< "                                " << endl
+							<< "     ????? ????? ???????        " << endl
+							<< "     ???? ??? ???? ??????       " << endl
+							<< "     ??? ???? ?? ? ?????????    " << endl
+							<< "     ??????? ???? ????? ?       " << endl
+							<< "<a>        <0> Back          <d>" << endl
+							<< endl
+							<< "             ";
+					}
+					break;
+				case 4:
+					if (dangers[1]) {
+						cout
+							<< "           Page 4/4             " << endl
+							<< "                                " << endl
+							<< "     Rooms are getting BIGGER,  " << endl
+							<< "     I barely see exit.         " << endl
+							<< "     Why I'm not starving?      " << endl
+							<< "     I hope it'll end soon.     " << endl
+							<< "<a>        <0> Back             " << endl
+							<< endl
+							<< "             ";
+					}
+					else {
+						cout
+							<< "           Page 4/4             " << endl
+							<< "                                " << endl
+							<< "     ????? ??? ??????? ???????  " << endl
+							<< "     ? ?????? ??? ?????         " << endl
+							<< "     ??? ??? ??? ?????????      " << endl
+							<< "     ? ???? ????? ??? ?????     " << endl
+							<< "<a>        <0> Back             " << endl
+							<< endl
+							<< "             ";
+					}
 					break;
 				}
 				direction = _getch();
 				if (direction == 'a') page--;
 				if (direction == 'd') page++;
-				if (page == 0) page = 1;
-				if (page > 2) page = 2;
+				if (page < 1) page = 1;
+				if (page > 4) page = 4;
 			}
 			break;
 		}
 		case '3': // skins
 		{
+			newskin = false;
 			int m = A.skinnumber;
 			direction = 'e';
 			while (direction != '0')
@@ -461,7 +576,7 @@ int main()
 				if (skins[3]) cout << hero_skins[3] << hero_skins[3] << "       "; else cout << "??       ";
 				if (skins[4]) cout << hero_skins[4] << hero_skins[4] << "       "; else cout << "??       ";
 				if (skins[5]) cout << hero_skins[5] << hero_skins[5] << "       " << endl; else cout << "??       " << endl;
-				cout << "     " << pos[3][0] << pos[3][1] << "       " << pos[4][0] << pos[4][1] << "       " << pos[5][0] << pos[5][1] << "       " << endl << endl;	
+				cout << "     " << pos[3][0] << pos[3][1] << "       " << pos[4][0] << pos[4][1] << "       " << pos[5][0] << pos[5][1] << "       " << endl << endl;
 				cout
 					<< "                             " << endl
 					<< "           <0> Back          " << endl
